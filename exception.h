6da4e712555569de49fc10539307a277ae5026c7
Wind0ws11Aero <jasonrex1999@icloud.com>
2026-07-02 13:20:29 +0800

    Refactored exception.h

--- exception.h
+++ exception.h
@@ -1,91 +1,120 @@
 #ifndef EXCEPTION_H
 #define EXCEPTION_H
 #include "oop.h"
+#include "oopclang.h"
+#include <_abort.h>
 #include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <_abort.h>
 
-typedef struct Exception
+class (Exception)
 {
     unsigned long long int errno;
     // A jmp_buf for setjmp and longjmp. Don't access it.
     jmp_buf *jbuf;
     const char *msg;
-} Exception;
+};
 
-typedef struct ExceptionHandler
+#define Exception(msg_)                                                                            \
+    ({                                                                                             \
+        Exception *e = malloc(sizeof(Exception));                                                  \
+        e->msg = msg_;                                                                             \
+        e->errno = 1;                                                                              \
+        e;                                                                                         \
+    })
+
+class (ExceptionHandler)
 {
     Exception *list;
     size_t len;
-} ExceptionHandler;
+    method(void, push, Exception);
+    method(Exception *, pop, void);
+    method(Exception *, peek, void);
+};
 
-thread_local ExceptionHandler _handler;
+ctor(ExceptionHandler)
+{
+    this->len = 0;
+    this->list = malloc(sizeof(Exception));
+    bind(this, push, ^(Exception e) {
+        ++this->len;
+        this->list = realloc(this->list, sizeof(Exception) * this->len);
+        this->list[this->len - 1] = e;
+    });
+    bind(this, pop, ^{
+        Exception *e = &this->list[--this->len];
+        this->list = realloc(this->list, sizeof(Exception) * (this->len ? this->len : 1));
+        return e;
+    });
+    bind(this, peek, ^{
+        return &this->list[this->len - 1];
+    });
+    return 0;
+};
+
+dtor(ExceptionHandler)
+{
+    free(this->list);
+};
+
+thread_local ExceptionHandler *_global_handler;
 
 thread_local Exception *_placeholder;
 
-[[gnu::constructor]] void _ct()
+[[gnu::constructor]] void _exception_init()
 {
-    _handler.len = 0;
-    _handler.list = malloc(sizeof(Exception));
-    _placeholder = malloc(sizeof(Exception));
+    _global_handler = new(ExceptionHandler);
+    _global_handler->len = 0;
+    _global_handler->list = malloc(sizeof(Exception));
+    _placeholder = Exception("");
 }
 
-[[gnu::destructor]] void _dt()
+[[gnu::destructor]] void _exception_deinit()
 {
-    free(_handler.list);
+    free(_global_handler->list);
+    free(_global_handler);
     free(_placeholder);
 }
 
-void ExceptionHandler_push(ExceptionHandler *this, Exception e)
-{
-    ++this->len;
-    this->list = realloc(this->list, sizeof(Exception) * this->len);
-    this->list[this->len - 1] = e;
-}
-
-Exception *ExceptionHandler_pop(ExceptionHandler *this)
-{
-    Exception *e = &this->list[--this->len];
-    this->list = realloc(this->list, sizeof(Exception) * (this->len ? this->len : 1));
-    return e;
-}
-
-Exception *ExceptionHandler_peek(ExceptionHandler *this)
-{
-    return &this->list[this->len - 1];
-}
-
-#define try                                                                                        \
+#define try_with_handler(_handler)                                                                                        \
     {                                                                                              \
         jmp_buf __jb;                                                                              \
         int e = setjmp(__jb);                                                                      \
         if (e == 0)                                                                                \
-            ExceptionHandler_push(&_handler, (Exception){.errno = 0, .jbuf = &__jb});              \
+            _handler->push((Exception){.errno = 0, .jbuf = &__jb});               \
         if (e == 0)
 
-#define catch                                                                                      \
+#define catch_with_handler(_handler)                                                                                      \
     }                                                                                              \
-    if (ExceptionHandler_peek(&_handler)->errno == 0) ExceptionHandler_pop(&_handler);\
-    else for (Exception *__exception = ExceptionHandler_peek(&_handler);                                 \
-         __exception->errno != 0 && __exception != _placeholder; __exception = _placeholder, ExceptionHandler_pop(&_handler))
+    if (_handler->peek()->errno == 0)                                               \
+        _handler->pop();                                                            \
+    else                                                                                           \
+        for (Exception *__exception = _handler->peek();                             \
+             __exception->errno != 0 && __exception != _placeholder;                               \
+             __exception = _placeholder, _handler->pop())
+
+#define try try_with_handler(_global_handler)
+#define catch catch_with_handler(_global_handler)
 
 #define exception_as(ename) Exception *ename = __exception
 
-#define Exception(msg_) ({Exception *e = malloc(sizeof(Exception)); e->msg = msg_; e->errno = 1; e;})
-
-[[noreturn]] void throw(Exception *e)
+[[noreturn]] void throw_with_handler(ExceptionHandler *_handler, Exception * e)
 {
-    if (_handler.len == 0)
+    if (_handler->len == 0)
     {
         perror("ExceptionHandler: terminating due to an uncaught exception. Aborting.\n");
         abort();
     }
-    Exception *oe = ExceptionHandler_peek(&_handler);
-    oe->errno = e->errno;
+    Exception *oe = _handler->peek();
+    oe->errno = e->errno; 
     oe->msg = e->msg;
     free(e);
     longjmp(*oe->jbuf, 1);
+}
+
+[[noreturn]] void throw(Exception *e)
+{
+    throw_with_handler(_global_handler, e);
 }
 
 // Annotation Hint Start
